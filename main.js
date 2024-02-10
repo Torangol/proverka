10b5eeae4ba76fd9e56c43e4367635117119b9b7
Torangol <124704154+Torangol@users.noreply.github.com>
2024-02-10 13:31:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+//вывод в консоль
+// console.log()
+
+//let = int
+
+
+// новый стандарт let и const, в старых var
+
+// let id = 10;
+// console.log(id);
+// id = 5;
+// console.log(id);
+
+// const нельзя менять, он 1
+
+// const bar = 10;
+// console.log(bar);
+
+//у var глобальная зона видимости
+// {
+//     let vadas = "asa"
+//     console.log(vadas)
+
+//     var soon = "sson";
+// }
+
+// console.log(soon)
+
+// null = ничего или пусто
+
+// let userName = 151;
+// userName = null;
+
+// undefined - ничего не задано
+
+// let black;
+// console.log(black)
+
+//оператор typeof
+// let bob = "Bob"
+// console.log(typeof bob)
+
+const time = 20;
