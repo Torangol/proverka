750472404516caceab63639eab2975442aa63f84
Torangol <124704154+Torangol@users.noreply.github.com>
2024-02-11 15:48:50 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -40,4 +40,71 @@
 // let bob = "Bob"
 // console.log(typeof bob)
 
-const time = 20;
+
+
+const time = 17;
+if (time < 12) {
+    console.log("Good morning")
+} else if (time >= 12 && time < 18){
+    console.log("Good day")
+} else {
+    console.log("Good night")
+}
+
+
+
+// function Run() {
+//     alert("Это ваши друзья? Советую найти побольше")
+// }
+
+function SayHi(name){
+    console.log(`Привет ${name}! Как поживаешь?`)
+}
+
+SayHi("KAKASKS");
+
+
+function sum(a, b){
+    const res = a + b;
+    return res;
+}
+
+function min(a, b){
+    const res = a - b;
+    return res;
+}
+
+function doSomething(func){
+    const res = func(x = 10, y = 80)
+    console.log(res);
+}
+
+doSomething(sum);
+doSomething(min);
+
+// самовызывающаяся фуникция
+(function user(name) {
+    console.log(`Hi ${name} My friend`)
+})("MAKAS");
+
+// стрелочная функция
+
+const SayBay = (name) => {
+    console.log(`Hello ${name}! How are you?`)
+}
+SayBay("Ilya");
+
+
+const sea = (a, b) => {
+    const res = a + b;
+    console.log (res);
+}
+sea(1,1);
+
+const sea1 = (a, b) => {
+    const res = a + b;
+    return res;
+}
+
+const mark = sea1(1,1);
+console.log(mark);
